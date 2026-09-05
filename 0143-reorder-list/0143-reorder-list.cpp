34cc68403a00013d73250e7dc30477d7097ab735
Ashwani Upadhyay <avinash744826@gmail.com>
2026-09-05 10:06:47 +0530

    Time: 0 ms (100%), Space: 22.9 MB (26.08%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.cpp
+++ 0143-reorder-list/0143-reorder-list.cpp
@@ -1,28 +1,44 @@
 class Solution {
 public:
     void reorderList(ListNode* head) {
-        if (!head || !head->next) return;
+        if(head == NULL || head->next == NULL)
+            return;
 
-        vector<ListNode*> nodes;
-        ListNode* curr = head;
-        while (curr != nullptr) {
-            nodes.push_back(curr);
-            curr = curr->next;
+        // 1. Find middle
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while(fast != NULL && fast->next != NULL) {
+            slow = slow->next;
+            fast = fast->next->next;
         }
 
-        int left = 0;
-        int right = nodes.size() - 1;
+        // 2. Reverse second half
+        ListNode* prev = NULL;
+        ListNode* curr = slow->next;
 
-        while (left < right) {
-            nodes[left]->next = nodes[right];
-            left++;
+        slow->next = NULL;  // first & second half separate
 
-            if (left == right) break;
-
-            nodes[right]->next = nodes[left];
-            right--;
+        while(curr != NULL) {
+            ListNode* next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
         }
 
-        nodes[left]->next = nullptr;
+        // 3. Merge alternate nodes
+        ListNode* first = head;
+        ListNode* second = prev;
+
+        while(second != NULL) {
+            ListNode* firstNext = first->next;
+            ListNode* secondNext = second->next;
+
+            first->next = second;
+            second->next = firstNext;
+
+            first = firstNext;
+            second = secondNext;
+        }
     }
 };
